6b8a562ceffc92dcb8f4621b5c26e336dfac46df
Vincent Ferries <vincent.ferries@gmail.com>
2018-02-01 14:17:29 +0100

    solution

---
+++ TP2/app/src/main/java/com/example/vincent/repodetail/RepositoryDetailActivity.java
@@ -0,0 +1,26 @@
+package com.example.vincent.repodetail;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RepositoryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository_detail);
+        Button githubButton = findViewById(R.id.githubButton);
+        githubButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/vferries/iut-android"));
+                startActivity(intent);
+            }
+        });
+    }
+}
